dae51e7aaabd60d9eee663d40524c38e4b480eaf
kairyerbylatov <kairerbylatov@gmail.com>
2022-07-08 13:50:51 +0600

    JS HW

---
+++ script/js.js
@@ -0,0 +1,179 @@
+//  1 - task
+//  var x = parseInt(prompt("Insert a number"));
+//  alert(x*x + 2*x - 1)
+
+//============
+
+// 2 -task
+// var x = parseInt(prompt("Insert number"));
+// var y = parseInt(prompt("Insert secound number"));
+// if(x / y == 0){
+//     console.log("Divisible");
+// }else{
+//     console.log("Not Divisible")
+// }
+
+//============
+
+// 3 -task
+// var a = prompt("Insert Name:")
+// var b = prompt("Insert Surname:")
+// var c = prompt("Insert City:")
+// var d = parseInt(prompt("Insert age:"));
+// alert("Name: " + a + "\n" + "Surname: " + b + "\n" + "City : " + c + "\n" + "age: " + d)
+
+//============
+// 4 - task
+// var a = parseInt(prompt("Insert num:"));
+// var b = parseInt(prompt("Insert num:"));
+// var c = parseInt(prompt("Insert num:"));
+// console.log(a + b + c);
+// console.log(a - b - c);
+// console.log(a * b * c);
+
+//============
+// 5 - task
+// var a = parseInt(prompt("Insert 1st num:"));
+// var b = parseInt(prompt("Insert 2nd num:"));
+
+// console.log(a % b);
+
+//============
+// 6  - task
+//  var a = parseInt(prompt("Insert 1st num:"));
+//  var b = parseInt(prompt("Insert 2nd num:"));
+//  console.log(Math.sqrt(a*a + b*b));
+
+//============
+// 7 - task
+//  var a = parseInt(prompt("Insert speed:"));
+//  var b = parseInt(prompt("Insert kilometrs:"));
+
+// if(b % a == 0){
+//     console.log("YES, YOU CAN REACH");
+// }else{
+//     console.log("NO, YOU CAN NOT REACH");
+// }
+
+//============
+//  8 - task
+//  wiskas = 120
+//  var a = parseInt(prompt("Insert wiskas:"));
+//  var b = a * 120
+//  if(b > 4000){
+//      console.log(b - b / 100 * 10);
+//  }else{
+//      console.log(b);
+//  }
+
+//============
+// 9 - task
+// var a = parseInt(prompt("INSERT YOUR AMOUNT IN KZT:"));
+// var b = parseInt(prompt("1-USD"+ "\n" + "2-EUR" + "\n" + "3-GBP"));
+
+// if(b == 1){
+//     console.log(a / 380); //USD
+// }if(b == 2){
+//     console.log(a / 430); //EUR
+// }if(b == 3){
+//     console.log(a / 480); //GBP
+// }
+
+//============
+// 10 - task
+// var a = prompt("Isert pass");
+// var b = "user qwerty"
+
+// if(a == b){
+//     console.log("Authentication completed");
+// }else{
+//     console.log("Authentication not completed");
+// }
+
+//============
+// 11 - task
+// var a = parseInt(prompt("INSERT AGE"));
+// if(a % 4 == 0 && a % 100 !=0){
+//     console.log("YES");
+// }else{
+//     console.log("NO");
+// }
+
+//============
+// 12 - task
+// var a = parseInt(prompt("INSERT NUM"));
+// if(a >= 90){
+//     console.log("A");
+// }else if(a >= 75){
+//     console.log("B");
+// }else if(a >= 60){
+//     console.log("C");
+// }else if(a >= 50){
+//     console.log("D");
+// }else{
+//     console.log("F");
+// }
+
+//============
+// 13 - task
+
+// var a = parseInt(prompt("Choose your branch:" +"\n" + "1-Science"+ "\n" + "2-Humanitarian subjects" + "\n" + "3-Art" + "\n" + "4-Sport"));
+// if(a == 1){
+//     var b = prompt("1-Math" + "\n" + "2-Physics")
+//     if(b == 1){
+//         alert("You are Financier");
+//     }else if(b ==2){
+//         alert("You are Engineer");
+//     }
+// }
+// if(a == 2){
+//     var b = prompt("1-History" + "\n" + "2-Foreign Languages")
+//     if(b == 1){
+//         alert("You are Historic or Diplomat  ");
+//     }else if(b ==2){
+//         alert("You are Translator");
+//     }
+// }
+// if(a == 3){
+//     var b = prompt("1-Drawing" + "\n" + "2-Singing")
+//     if(b == 1){
+//         alert("You are Painter or Architect");
+//     }else if(b ==2){
+//         alert("You are Singer or Tamada");
+//     }
+// }
+// if(a == 4){
+//     var b = prompt("1-Team" + "\n" + "2-Individual")
+//     if(b == 1){
+//         alert("You are footballer or Basketball player");
+//     }else if(b ==2){
+//         alert(" You are boxer or tennis player");
+//     }
+// }
+
+//============
+// 14 - task
+// var a = parseInt(prompt("INSERT 1ST NUM"));
+// var b = parseInt(prompt("INSERT 2ND NUM"));
+// var c = parseInt(prompt("INSERT 3RD NUM"));
+
+// if(a + b > c && a + c > b && b + c > a){
+//     console.log("YES");
+// }else{
+//     console.log("NO");
+// }
+
+//============
+// 15 - task
+
+// var a = prompt("INSERT NUM");
+// var hour = 60;
+// if(a % hour == 0){
+//     console.log("ONLY " + a/hour + " hours");
+// }else{
+//     result = a/hour;
+//     clear = parseInt(result);
+//     min = parseInt((result - clear)*60);
+//     console.log(clear + " HOURS and  " + min+ " MINUTES");
+// }
+
